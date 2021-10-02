825e46c38ab682a3a899921b1e59e20ec7784b7f
skorokhodtatiana <tanjakharza@mail.ru>
2021-10-02 17:33:33 +0300

    in workin

---
+++ 1-task-try-catch/try-catch.js
@@ -0,0 +1,37 @@
+function getGifs() {
+    let valInputSearch = document.getElementById('searchGif').value;
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=7wJgSFYwhi8JAM6vE1UxJ7gaTTomMPmR&q=" + valInputSearch + "&limit=5&offset=0&rating=g&lang=en")
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Something went wrong');
+            }
+        })
+        .then(responseImg => {
+            console.log(responseImg.data.length)
+            for (let i = 0; i < responseImg.data.length; i++) {
+                let imgGif = document.createElement('img');
+                document.body.append(imgGif);
+                imgGif.src = responseImg.data[i].images.fixed_height_small.url;
+            }
+        })
+        .catch((error) => {
+            console.log(error.name)
+        });
+}
+
+
+// fetch(url).then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     } else {
+//       throw new Error('Something went wrong');
+//     }
+//   })
+//   .then((responseJson) => {
+//     // Do something with the response
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//   });

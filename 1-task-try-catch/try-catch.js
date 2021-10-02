82e89c34d454d9547bd7aa4eb67f23c42721bc8c
skorokhodtatiana <tanjakharza@mail.ru>
2021-10-03 00:49:35 +0300

    added check internet

--- 1-task-try-catch/try-catch.js
+++ 1-task-try-catch/try-catch.js
@@ -1,13 +1,7 @@
 function getGifs() {
     let valInputSearch = document.getElementById('searchGif').value;
     fetch("https://api.giphy.com/v1/gifs/search?api_key=7wJgSFYwhi8JAM6vE1UxJ7gaTTomMPmR&q=" + valInputSearch + "&limit=5&offset=0&rating=g&lang=en")
-        .then((response) => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('Something went wrong');
-            }
-        })
+        .then(response => response.json())
         .then(responseImg => {
             console.log(responseImg.data.length)
             for (let i = 0; i < responseImg.data.length; i++) {
@@ -16,9 +10,15 @@ function getGifs() {
                 imgGif.src = responseImg.data[i].images.fixed_height_small.url;
             }
         })
+        // .catch((error) => {
+        //     throw new Error('Something went wrong')
+        // });
+
         .catch((error) => {
-            console.log(error.name)
-        });
+            if(!navigator.onLine) {
+              alert('Отсутствует подключение к сети интернет');
+            }
+          });
 }
 
 
